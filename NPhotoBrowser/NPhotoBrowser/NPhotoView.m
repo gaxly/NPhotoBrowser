f4aab76bb669ab474339035c46f3eb0760a6fc37
gaxly <www.gaxly@foxmail.com>
2017-02-27 16:38:18 +0800

    delete something

--- NPhotoBrowser/NPhotoBrowser/NPhotoView.m
+++ NPhotoBrowser/NPhotoBrowser/NPhotoView.m
@@ -24,30 +24,6 @@ const CGFloat photoViewMaxScale = 3;
 
 @implementation NPhotoView
 
-//- (instancetype)initWithFrame:(CGRect)frame {
-//    self = [super initWithFrame:frame];
-//    if (self) {
-//        self.bouncesZoom = YES;
-//        self.maximumZoomScale = photoViewMaxScale;
-//        self.multipleTouchEnabled = YES;
-//        self.showsVerticalScrollIndicator = YES;
-//        self.showsHorizontalScrollIndicator = YES;
-//        self.delegate = self;
-//        
-//        _imageView = [[YYAnimatedImageView alloc] init];
-//        _imageView.backgroundColor = [UIColor darkGrayColor];
-//        _imageView.contentMode = UIViewContentModeScaleAspectFill;
-//        _imageView.clipsToBounds = YES;
-//        [self addSubview:_imageView];
-//        [self resizeImageView];
-//        
-//        _progressLayer = [[NProgressLayer alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
-//        _progressLayer.position = CGPointMake(frame.size.width/2, frame.size.height/2);;
-//        _progressLayer.hidden = YES;
-//        [self.layer addSublayer:_progressLayer];
-//    }
-//    return self;
-//}
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
@@ -74,50 +50,6 @@ const CGFloat photoViewMaxScale = 3;
     return self;
 }
 
-//- (void)setPhoto:(NPhoto *)photo determinate:(BOOL)determinate {
-//    _photo = photo;
-//    [_imageView yy_cancelCurrentImageRequest];
-//    if (photo) {
-//        if (photo.image) {
-//            _imageView.image = photo.image;
-//            NSLog(@"-- %@",NSStringFromCGRect(_imageView.frame));
-//            _photo.finished = YES;
-//            [_progressLayer stopSpin];
-//            _progressLayer.hidden = YES;
-//            [self resizeImageView];
-//            return;
-//        }
-//        __weak typeof(self) wself = self;
-//        YYWebImageProgressBlock progressBlock = nil;
-//        if (determinate) {
-//            progressBlock = ^(NSInteger receivedSize, NSInteger expectedSize) {
-//                __strong typeof(wself) sself = wself;
-//                double progress = (double)receivedSize / expectedSize;
-//                sself.progressLayer.hidden = NO;
-//                sself.progressLayer.strokeEnd = MAX(progress, 0.01);
-//            };
-//        } else {
-//            [_progressLayer startSpin];
-//        }
-//        _progressLayer.hidden = NO;
-//        
-//        _imageView.image = photo.thumbImage;
-//        [_imageView yy_setImageWithURL:photo.imageUrl placeholder:photo.thumbImage options:kNilOptions progress:progressBlock transform:nil completion:^(UIImage *image, NSURL *url, YYWebImageFromType from, YYWebImageStage stage, NSError *error) {
-//            __strong typeof(wself) sself = wself;
-//            if (stage == YYWebImageStageFinished) {
-//                [sself resizeImageView];
-//            }
-//            [sself.progressLayer stopSpin];
-//            sself.progressLayer.hidden = YES;
-//            sself.photo.finished = YES;
-//        }];
-//    } else {
-//        [_progressLayer stopSpin];
-//        _progressLayer.hidden = YES;
-//        _imageView.image = nil;
-//    }
-//    [self resizeImageView];
-//}
 
 - (void)setPhoto:(NPhoto *)photo determinate:(BOOL)determinate {
     _photo = photo;
@@ -125,7 +57,6 @@ const CGFloat photoViewMaxScale = 3;
     if (photo) {
         if (photo.image) {
             _imageView.image = photo.image;
-            NSLog(@"-- %@",NSStringFromCGRect(_imageView.frame));
             _photo.finished = YES;
             [_progressLayer stopSpin];
             _progressLayer.hidden = YES;
@@ -164,33 +95,6 @@ const CGFloat photoViewMaxScale = 3;
     [self resizeImageView];
 }
 
-//- (void)resizeImageView {
-//    if (_imageView.image) {
-//        NSLog(@"-- %@",NSStringFromCGRect(_imageView.frame));
-//        CGSize imageSize = _imageView.image.size;
-//        CGFloat width = _imageView.frame.size.width;
-//        CGFloat height = width * (imageSize.height / imageSize.width);
-//        CGRect rect = CGRectMake(0, 0, width, height);
-//        _imageView.frame = rect;
-//        
-//        // If image is very high, show top content.
-//        if (height <= self.bounds.size.height) {
-//            _imageView.center = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
-//        } else {
-//            _imageView.center = CGPointMake(self.bounds.size.width/2, height/2);
-//        }
-//        
-//        // If image is very wide, make sure user can zoom to fullscreen.
-//        if (width / height > 2) {
-//            self.maximumZoomScale = self.bounds.size.height / height;
-//        } else {
-//            CGFloat width = self.frame.size.width - 2 * photoViewPadding;
-//            _imageView.frame = CGRectMake(0, 0, width, width * 2 / 3);
-//            _imageView.center = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
-//        }
-//    }
-//    self.contentSize = _imageView.frame.size;
-//}
 
 - (void)resizeImageView {
     if (_imageView.image) {
